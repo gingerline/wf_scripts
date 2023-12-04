ae08028888ffdfc201b5d27129678b66f853d41c
Sugandha Kapoor <sugandhak.email@gmail.com>
2023-12-04 15:28:17 -0500

    get outseta key from url

--- em_intg_keys_j7.js
+++ em_intg_keys_j7.js
@@ -1,26 +1,20 @@
-// global constants
-const BASE_URL = "https://apis.emno.io/";
-const COLLECTION_API_URL = BASE_URL + "collections";
-const COLLECTION_DETAILS_URL = "https://emno-v1.webflow.io/app/collections-details?collectionId=";
-
-// attaching listeners to all modal input boxes for delete collection
-(function keyChangeDeleteCollectionInput() {
-    const inputTexts = document.getElementsByClassName("delete-collection-input");
+// attaching listeners to all modal input boxes for delete key
+(function keyChangeDeleteKeyInput() {
+    const inputTexts = document.getElementsByClassName("delete-key-input");
 
     for (let inputEl of inputTexts) {
         inputEl.addEventListener("keyup", event => {
 
-            //const activeFormElement = document.querySelector('#delete-popup[style="display: block; opacity: 1;"] #wf-form-Delete-Collection');
-            const em_collectionId = event.target.getAttribute("em-collectionId");
-            const selector = "#wf-form-Delete-Collection[em-collectionId=\"" + em_collectionId + "\"]";
+            const em_keyId = event.target.getAttribute("em-keyId");
+            const selector = "#wf-form-Delete-Key[em-keyId=\"" + em_keyId + "\"]";
             const activeFormElement = document.querySelector(selector);
 
             if (activeFormElement) { // Only proceed if the activeFormElement exists.
                 const submitButton = activeFormElement.querySelector(".popup-form-error-button");
                 submitButton.disabled = true;
                 submitButton.classList.add("disabled");
-                const inputText = activeFormElement.querySelector("#delete-collection-input");
-                var actualItemName = inputText.getAttribute("collection-item").trim();
+                const inputText = activeFormElement.querySelector("#delete-key-input");
+                var actualItemName = inputText.getAttribute("em-keyName").trim();
 
                 var inputVal = inputText.value.trim();
                 if (inputVal === actualItemName) {
@@ -43,20 +37,19 @@ function getNextSiblingWithClass(element, className) {
     }
     return null;
 }
-// attaching listeners to delete collection buttons
+// attaching listeners to delete key buttons
 function openDeletePopupClickHandler(event) {
     // Set the initial state for all sibling elements
-    const elem = event.target;
-    const em_collectionName = elem.getAttribute("em-collectionName");
-    const em_collectionId = elem.getAttribute("em-collectionId");
+    //const targetElem = event.target;
+    let elem = event.target.closest(".action-link-table");
+    const em_keyName = elem.getAttribute("em-keyName");
+    const em_keyId = elem.getAttribute("em-keyId");
     const deleteModal = document.getElementById("delete-popup");
     if (posthog.isFeatureEnabled('enable-emEvents')) {
-        posthog.capture('user_clicked_delete_collection', {
+        posthog.capture('user_clicked_delete_intgKey', {
             source: "ui"
         });
     }
-
-
     if (deleteModal) {
 
         deleteModal.classList.add('add-animation');
@@ -64,24 +57,23 @@ function openDeletePopupClickHandler(event) {
         deleteModal.style.opacity = "100%";
     }
 
-    const inputElement = deleteModal.querySelector("#delete-collection-input");
+    const inputElement = deleteModal.querySelector("#delete-key-input");
     if (inputElement) {
-        inputElement.setAttribute("collection-item", em_collectionName);
-        inputElement.setAttribute("em-collectionId", em_collectionId);
+        inputElement.setAttribute("em-keyName", em_keyName);
+        inputElement.setAttribute("em-keyId", em_keyId);
     }
 
     // Find the text element with id "confirm-text" and replace its text content
     const confirmText = deleteModal.querySelector("#confirm-text");
-    if (confirmText && em_collectionName) {
-        confirmText.textContent = `Are you sure you want to delete "${em_collectionName}"?`;
+    if (confirmText && em_keyName) {
+        confirmText.textContent = `Are you sure you want to delete "${em_keyName}"?`;
     }
 
 
-    const form = deleteModal.querySelector("#wf-form-Delete-Collection");
-
+    const form = deleteModal.querySelector("#wf-form-Delete-Key");
 
     if (form) {
-        form.setAttribute("em-collectionId", em_collectionId);
+        form.setAttribute("em-keyId", em_keyId);
         // Reset the inputs in the form
         form.reset();
         const submitButton = form.querySelector(".popup-form-error-button");
@@ -105,29 +97,24 @@ function openDeletePopupClickHandler(event) {
     }
 
     // finally close the dropdown menu
-    openDropdownClickHandler(event);
+    //openDropdownClickHandler(event);
 
 }
 
 
-// attaching listeners to edit collection buttons
-function openEditPopupClickHandler(event) {
+// attaching listeners to edit key buttons
+function openAddPopupClickHandler(event) {
     // Set the initial state for all sibling elements
     const elem = event.target;
-    const em_collectionName = elem.getAttribute("em-collectionName");
-    const em_collectionId = elem.getAttribute("em-collectionId");
-    const em_collectionDescription = elem.getAttribute("em-collectionDescription");
-    const editModal = document.getElementById("edit-popup");
+    const addModal = document.getElementById("add-popup");
+
+    const form = addModal.querySelector("#wf-form-Add-Key");
     if (posthog.isFeatureEnabled('enable-emEvents')) {
-        posthog.capture('user_clicked_edit_collection', {
+        posthog.capture('user_clicked_new_intgKey', {
             source: "ui"
         });
     }
-
-
-    const form = editModal.querySelector("#wf-form-Edit-Collection");
     if (form) {
-        form.setAttribute("em-collectionId", em_collectionId);
         // Reset the inputs in the form
         form.reset();
         // Show the form
@@ -147,80 +134,16 @@ function openEditPopupClickHandler(event) {
     }
 
 
-    // Find the input element with the name attribute equal to "name"
-    var nameInput = editModal.querySelector('input[name="name"]');
-    // Check if the input element exists before setting its value
-    if (nameInput) {
-        nameInput.value = em_collectionName;
-    }
-
-    // Find the textarea element with the name attribute equal to "description" within the form
-    var textarea = editModal.querySelector('textarea[name="description"]');
-    // Check if the textarea element exists within the form
-    if (textarea && em_collectionDescription) {
-        // Set the value of the textarea to "abc"
-        textarea.value = em_collectionDescription;
-    }
-
-    editModal.classList.add('add-animation');
-    editModal.style.display = "block";
-    editModal.style.opacity = "100%";
+    addModal.classList.add('add-animation');
+    addModal.style.display = "block";
+    addModal.style.opacity = "100%";
 
     // finally close the dropdown menu
-    openDropdownClickHandler(event);
-
-}
-
-// attaching listeners to edit collection buttons
-function addClickHandler(event) {
-    // Set the initial state for all sibling elements
-    const elem = event.target;
-    if (posthog.isFeatureEnabled('enable-emEvents')) {
-        posthog.capture('user_clicked_add_collection', {
-            source: "ui"
-        });
-    }
+    // openDropdownClickHandler(event);
 
 }
 
 
-// attaching listeners to all delete collection buttons
-function openDropdownClickHandler(event) {
-    // Set the initial state for all sibling elements
-    let targetDropdownWrapper = event.target.closest(".dropdown-wrapper");
-
-
-
-    // Get all dropdown wrappers
-    var allDropdownWrappers = document.querySelectorAll(".dropdown-wrapper");
-    // Close all other dropdowns except for the one that was clicked
-    allDropdownWrappers.forEach(function (wrapper) {
-        if (wrapper !== targetDropdownWrapper) {
-            var dropdownContent = wrapper.querySelector(".dropdown-content");
-            if (dropdownContent) {
-                dropdownContent.style.display = "none";
-                dropdownContent.style.opacity = "0%";
-            }
-        }
-    });
-
-    const targetDropdownContent = targetDropdownWrapper.getElementsByClassName("dropdown-content");
-    if (targetDropdownContent.length > 0) {
-        if (targetDropdownContent[0].style.display === "none" || targetDropdownContent[0].style.display === "") {
-
-            targetDropdownContent[0].classList.add('add-animation');
-            targetDropdownContent[0].style.display = "block";
-            targetDropdownContent[0].style.opacity = "100%";
-        }
-        else {
-            targetDropdownContent[0].style.display = "none";
-            targetDropdownContent[0].style.opacity = "0%";
-        }
-
-
-    }
-
-}
 
 function getOutsetaKey() {
     return sessionStorage.getItem("Outseta.nocode.accessToken");
@@ -260,8 +183,9 @@ Webflow.push(function () {
         }, {});
 
         console.log(formDataObj);
-        const collectionId = $form.attr('em-collectionId');
-        const finalActionURL = formActionURL + '/' + collectionId;
+        debugger;
+        const keyId = $form.attr('em-keyId');
+        let finalActionURL = formActionURL;
 
         let outsetaJWT = getOutsetaKey();
 
@@ -271,24 +195,27 @@ Webflow.push(function () {
         };
         console.log("Headers for the request:", headersData);
         console.log(JSON.stringify({
-            name: formDataObj.name,
-            description: formDataObj.description
+            name: "OpenAI-Key",
+            token: formDataObj.value
         }));
 
-
-
         var finalData = JSON.stringify({
-            "name": formDataObj.name,
-            "description": formDataObj.description
+            "name": "OpenAI-Key",
+            "token": formDataObj.value,
+            "type": "INTEGRATION",
+            "system": "OPEN-AI"
         });
 
         const formId = $form.attr("id");
         // change the formMethodType if its delete
-        if (formId === "wf-form-Delete-Collection") {
-            formMethodType = "delete";
-            finalData = {};
+        if (formId === "wf-form-Delete-Key") {
+            finalActionURL = formActionURL;
+            //formMethodType = "delete";
+            finalData = JSON.stringify({
+                "id": parseInt(keyId)
+            });
             if (posthog.isFeatureEnabled('enable-emEvents')) {
-                posthog.capture('user_submitted_delete_collection', {
+                posthog.capture('user_submitted_delete_intgKey', {
                     source: "ui",
                     form: formId,
                     formData: finalData
@@ -296,13 +223,14 @@ Webflow.push(function () {
             }
         } else {
             if (posthog.isFeatureEnabled('enable-emEvents')) {
-                posthog.capture('user_submitted_edit_collection', {
+                posthog.capture('user_submitted_new_intgKey', {
                     source: "ui",
                     form: formId,
                     formData: finalData
                 });
             }
         }
+
 
 
         $.ajax(finalActionURL, {
@@ -317,10 +245,9 @@ Webflow.push(function () {
                     .hide() // optional hiding of form
                     .siblings('.w-form-done').show() // Show success
                     .siblings('.w-form-fail').hide(); // Hide failure
-
-                if (formId === "wf-form-Delete-Collection") {
+                if (formId === "wf-form-Delete-emKey") {
                     if (posthog.isFeatureEnabled('enable-emEvents')) {
-                        posthog.capture('user_deleted_collection', {
+                        posthog.capture('user_deleted_intgKey', {
                             source: "ui",
                             form: formId,
                             formData: finalData
@@ -328,7 +255,7 @@ Webflow.push(function () {
                     }
                 } else {
                     if (posthog.isFeatureEnabled('enable-emEvents')) {
-                        posthog.capture('user_edited_collection', {
+                        posthog.capture('user_created_new_intgKey', {
                             source: "ui",
                             form: formId,
                             formData: finalData
@@ -371,6 +298,7 @@ Webflow.push(function () {
 window.addEventListener("DOMContentLoaded", reloadData);
 
 async function reloadData() {
+    // dom elements
     const contentTableDiv = document.getElementById("content-mid-table");
     const contentSectionDiv = document.getElementById("content-mid-starter");
     const spinner = document.getElementById("spinners");
@@ -383,16 +311,21 @@ async function reloadData() {
     // movieGrid.style.opacity = "0%";
     document.body.style.overflow = "hidden";
 
-
+    // global constants
+    const API_URL = "https://apis.emno.io/tokens";
+    debugger;
     // functions
-    async function fetchDataAndHandleResponse(URL) {
+    async function fetchDataAndHandleResponse() {
         try {
-            let response = await fetch(URL, {
-                method: 'GET',
+            let response = await fetch(API_URL, {
+                method: 'POST',
                 headers: {
                     ...headersData,
                     'Content-Type': 'application/json'
-                }
+                },
+                body: JSON.stringify({
+                    type: 'INTEGRATION'
+                })
             });
 
             if (!response.ok) {
@@ -420,75 +353,61 @@ async function reloadData() {
     }
 
     // get data
-    const em_collections = await fetchDataAndHandleResponse(COLLECTION_API_URL);
+    const em_keys = await fetchDataAndHandleResponse();
 
     // stop executing code if error fetching data
-    if (!em_collections) {
+    if (!em_keys) {
         errorDetected();
         return;
     }
-    function duplicateCollectionCard() {
+
+    function duplicateKeyRow() {
         // Clone the element and all its children using jQuery
-        const clonedElement = $('#collection-wrapper').clone(true);
-        clonedElement.attr('id', 'new-collection-wrapper');
+        const clonedElement = $('#key-wrapper').clone(true);
+        clonedElement.attr('id', 'new-key-wrapper');
         // Append the cloned element to the same parent as the original using jQuery
         clonedElement.appendTo('#table-wrapper');
 
 
-        const deleteMenu = clonedElement.find(".delete-coll-button");
-        deleteMenu.on("click", openDeletePopupClickHandler);
+        const deleteButton = clonedElement.find(".delete-key-button");
+        deleteButton.on("click", openDeletePopupClickHandler);
 
-        const editMenu = clonedElement.find(".edit-coll-button");
-        editMenu.on("click", openEditPopupClickHandler);
-
-        const dropdownTrigger = clonedElement.find(".dropdown-trigger");
-        dropdownTrigger.on("click", openDropdownClickHandler);
+        //const addButton = clonedElement.find(".add-key-button");
+        //addButton.on("click", openAddPopupClickHandler);
 
         return clonedElement;
     }
 
     // Check if the contentTableDiv has a child element with class "w-dyn-empty"
-    if (em_collections && em_collections.length > 0) {
+    if (em_keys && em_keys.length > 0) {
         // If it has the class, show the contentSectionDiv div and hide the contentTableDiv
         contentTableDiv.style.display = "block";
         contentSectionDiv.style.display = "none";
         spinner.style.display = "none";
         $("#table-wrapper").empty();
 
+
         // iterate through data results
         // create img element for each data item
         // add class to each image (class exists in Webflow)
         // append each item to movie grid
-        em_collections.forEach((em_coll) => {
-            const clonedElement = duplicateCollectionCard();
+        em_keys.forEach((em_key) => {
+            const clonedElement = duplicateKeyRow();
 
             // Update the div with ID "coll-name" within the cloned element
-            clonedElement.find("#coll-name").text(em_coll.name);
-            let collectionHref = COLLECTION_DETAILS_URL + em_coll.id;
-            clonedElement.find("#coll-name").attr("href", collectionHref);
+            clonedElement.find("#key-name").text(em_key.name);
 
             // Update the div with ID "coll-description" within the cloned element
-            clonedElement.find("#coll-description").text(em_coll.description);
+            clonedElement.find("#key-value").text(em_key.token);
 
-            // Update the div with ID "coll-dimension" within the cloned element
-            clonedElement.find("#coll-dimension").text(em_coll.config.dim);
+            clonedElement.attr('em-keyId', em_key.id);
 
-            // Update the div with ID "coll-dimension" within the cloned element
-            clonedElement.find("#coll-id").text(em_coll.id);
-
-            clonedElement.attr('em-collectionId', em_coll.id);
-            clonedElement.attr('em-collectionName', em_coll.name);
-            clonedElement.attr('em-collectionDescription', em_coll.description);
-
-
-            clonedElement.find(".action-link").attr('em-collectionId', em_coll.id);
-            clonedElement.find(".action-link").attr('em-collectionName', em_coll.name);
-
-            // also append the description if it the edit button.
-            clonedElement.find('.action-link.edit-coll-button').attr('em-collectionDescription', em_coll.description);
+            clonedElement.find(".action-link-table").attr('em-keyId', em_key.id);
+            clonedElement.find(".action-link-table").attr('em-keyName', em_key.name);
 
             // Show the cloned element (assuming it was hidden before)
-            clonedElement.show();
+            //clonedElement.show();
+            clonedElement.removeClass("key-wrapper");
 
         });
     } else {
@@ -498,8 +417,11 @@ async function reloadData() {
         spinner.style.display = "none";
     }
 
+
+
     // attaching listeners to add key buttons
-    $(".add-collection-btn").on("click", addClickHandler);
+    $(".add-key-button").on("click", openAddPopupClickHandler);
+
 
     // // remove loader and show movie grid
     setTimeout(() => {
